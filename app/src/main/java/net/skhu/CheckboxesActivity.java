b7b023d110667c8d216669ce37d304ca3446f544
kihyojeo <kihyojeo@gmail.com>
2022-04-04 17:39:46 +0900

    최초 커밋

---
+++ app/src/main/java/net/skhu/CheckboxesActivity.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheckboxesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkboxes);
+    }
+}
